04a8539c16801b0da6aae6f1bb805c5f198b3259
manavs19 <manavcool.sethi@gmail.com>
2015-04-05 15:25:13 +0530

    EventQueues defined

--- DistrSim/eventqueues.cpp
+++ DistrSim/eventqueues.cpp
@@ -1,5 +1,10 @@
 #include "eventqueues.h"
 
-EventQueues::EventQueues()
+EventQueues::EventQueues(QList<QQueue<EventAbstract*> > evQueue, QQueue<EventAbstract*> localQueue, QQueue<EventAbstract*> sendQueue, QList<NodeAbstract*> nodeList, QList<QList<QPair<EdgeAbstract*,int> > > edgeList)
 {
+    this->evQueue = evQueue;
+    this->localQueue = localQueue;
+    this->sendQueue = sendQueue;
+    this->nodeList = nodeList;
+    this->edgeList = edgeList;
 }
