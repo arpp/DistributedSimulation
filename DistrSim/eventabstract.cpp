2a030ca774f157f16c016cf644ac04b378cfcb28
arpp <arp8900@gmail.com>
2015-04-05 15:13:51 +0530

    EventAbstract done

--- DistrSim/eventabstract.cpp
+++ DistrSim/eventabstract.cpp
@@ -2,4 +2,14 @@
 
 EventAbstract::EventAbstract(NodeAbstract *n, unsigned long ts)
 {
+    this->node=n;
+    this->timestamp=ts;
+}
+
+unsigned long EventAbstract::getNodeID(){
+    return this->node->getNodeId();
+}
+
+unsigned long EventAbstract::getTimestamp(){
+    return this->timestamp;
 }
