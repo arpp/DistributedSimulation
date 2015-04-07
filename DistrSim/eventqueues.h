16dac6cf01d603888eb853c63ef01dc22bada48e
arpp <arp8900@gmail.com>
2015-04-07 15:51:06 +0530

    Modified queues

--- DistrSim/eventqueues.h
+++ DistrSim/eventqueues.h
@@ -6,11 +6,12 @@
 #include <QMap>
 #include <QQueue>
 #include "event.h"
+#include "eventdata.h"
 
 class EventQueues
 {
 public:
-    EventQueues(QMap<int,QQueue<Event*> > evQueue, QQueue<Event*> sendQueue, QList<NodeAbstract*> nodeList, QList<QList<QPair<NodeAbstract*,int> > > edgeList);
+    EventQueues(QMap<int,QQueue<Event*> > evQueue, QQueue<EventData*> sendQueue, QList<NodeAbstract*> nodeList, QList<QList<QPair<NodeAbstract*,int> > > edgeList);
     void addToEventQueue(Event* e);
     void addToSendQueue(Event* e);
     Event* getNextEvent();
@@ -18,8 +19,8 @@ public:
 
 private:
     QMap<int, QQueue<Event*> > evQueue;
+    QQueue<EventData*> sendQueue;
 
-    QQueue<Event*> sendQueue;
     QList<NodeAbstract*> nodeList;
     QList<QList<QPair<NodeAbstract*,int> > > edgeList;
 };
