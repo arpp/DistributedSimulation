87af2e15125dd16f060f7122d2d6d245eb1f1f86
arpp <arp8900@gmail.com>
2015-04-07 17:20:42 +0530

    Proper mutexes and some portion of the actual code

--- DistrSim/eventdata.cpp
+++ DistrSim/eventdata.cpp
@@ -1,9 +1,10 @@
 #include "eventdata.h"
 
-EventData::EventData(unsigned long ts, unsigned long nodeId)
+EventData::EventData(unsigned long ts, unsigned long nodeId, int type)
 {
     this->timestamp = ts;
     this->nodeId = nodeId;
+    this->type = type;
 }
 
 unsigned long EventData::getNodeId()
@@ -16,16 +17,21 @@ unsigned long EventData::getTimestamp()
     return this->timestamp;
 }
 
+int EventData::getType(){
+    return this->type;
+}
+
 QDataStream& operator<<(QDataStream& out, EventData &eventData)
 {
-    out << (quint64)(eventData.getTimestamp()) << (quint64)(eventData.getNodeId());
+    out << (quint64)(eventData.getTimestamp()) << (quint64)(eventData.getNodeId()) << eventData.getType();
     return out;
 }
 
 QDataStream& operator>>(QDataStream& in, EventData & eventData)
 {
     quint64 ts, nodeId;
-    in >> ts >> nodeId;
-    eventData = EventData((unsigned long)ts, (unsigned long)nodeId);
+    int type;
+    in >> ts >> nodeId >> type;
+    eventData = EventData((unsigned long)ts, (unsigned long)nodeId, type);
     return in;
 }
