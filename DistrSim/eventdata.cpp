289063c2811d534dae9aa5c517de87790f636385
manavs19 <manavcool.sethi@gmail.com>
2015-04-07 15:59:27 +0530

    Serialization done in EventData

--- DistrSim/eventdata.cpp
+++ DistrSim/eventdata.cpp
@@ -5,3 +5,27 @@ EventData::EventData(unsigned long ts, unsigned long nodeId)
     this->timestamp = ts;
     this->nodeId = nodeId;
 }
+
+unsigned long EventData::getNodeId()
+{
+    return this->nodeId;
+}
+
+unsigned long EventData::getTimestamp()
+{
+    return this->timestamp;
+}
+
+QDataStream& operator<<(QDataStream& out, EventData &eventData)
+{
+    out << (quint64)(eventData.getTimestamp()) << (quint64)(eventData.getNodeId());
+    return out;
+}
+
+QDataStream& operator>>(QDataStream& in, EventData & eventData)
+{
+    quint64 ts, nodeId;
+    in >> ts >> nodeId;
+    eventData = EventData((unsigned long)ts, (unsigned long)nodeId);
+    return in;
+}
--- DistrSim/eventdata.h
+++ DistrSim/eventdata.h
@@ -1,12 +1,17 @@
 #ifndef EVENTDATA_H
 #define EVENTDATA_H
 
+#include <QDataStream>
+
 class EventData
 {
 public:
     EventData(unsigned long ts, unsigned long nodeId);
     unsigned long getTimestamp();
     unsigned long getNodeId();
+
+    friend QDataStream& operator<<(QDataStream& out, EventData &eventData);
+    friend QDataStream& operator>>(QDataStream& in, EventData & eventData);
 
 private:
     unsigned long timestamp;
